27a37a5fbf34a5d337cf06582680f79af4bd8603
Yu Hsuan Chen <yu-chen@made-f0Ar7s7.clusters.42paris.fr>
2024-08-14 14:57:50 +0200

    adding structs

--- src/cube.c
+++ src/cube.c
@@ -1,9 +1,12 @@
+#include "../includes/cub3D.h"
+
 int main(int ac, char **av)
 {
 	t_data	data;
 
-	if ()
-	if (!parsing())
+	if (ac != 2 || !check_filetype())
 		return (0);
-	exec();
+	if (!parsing(&data))
+		return (0);
+	exec(&data);
 }
